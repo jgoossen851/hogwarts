d5f44977a5841341bc0035d3417e3db26ca636cc
Jeremy Goossen <jeremyg995@gmail.com>
2021-08-26 23:46:57 -0500

    Adds the Mozilla Public License Version 2.0 to the project.

--- installer_linux.sh
+++ installer_linux.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021, Jeremy Goossen jeremyg995@gmail.com
+
+
 # ToDo: Add flags (-g, -u, etc.) to enable skipping the interactive part
 
 # Ensure an argument was passed
